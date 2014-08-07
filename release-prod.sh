e4f73b907078da815a720bedc1227280465b770e
dsantos <dsantos@despegar.com>
2014-08-07 18:56:38 -0300

    cambios en script de release

--- release-prod.sh
+++ release-prod.sh
@@ -1,3 +1,14 @@
+
+echo "reseting git"
+git reset --hard
+echo "reset finished"
+
+echo "pulling changes"
+git pull
+echo "changes pulled"
+
+
+echo "updating config files"
 mv index.php index.dev.php
 sed '0,/development/s/development/production/' index.dev.php > index.php
 rm index.dev.php
@@ -5,7 +16,10 @@ rm index.dev.php
 mv .htaccess .htaccess.dev
 sed '/RewriteBase/d' .htaccess.dev > .htaccess
 rm .htaccess.dev
+echo "config files updated"
 
-
+echo "seting permissions in folders"
 chmod -R 777 assets/cache/
 chmod -R 777 assets/uploads/
+chmod -R 777 players/
+echo "set permisions finished"
